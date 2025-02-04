38f78101e25aee9281fedab67dcb5ab20314c457
Tanner Grammar <tanner@tannergrammar.com>
2025-02-04 10:08:55 -0700

    #1 - Adding first content info

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is my first full page in React on Home.tsx</p>
+    </div>
+  );
+}
+
+export default Home;

0e704ebecaba0eaf9a96884e86318c25fd6d023b
Tanner Grammar <tanner@tannergrammar.com>
2025-02-04 20:30:24 -0700

    add storybook and tailwind

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1 className="font-light text-green-500">Welcome to My Portfolio</h1>
       <p>This is my first full page in React on Home.tsx</p>
     </div>
   );
